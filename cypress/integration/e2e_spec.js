7c166e8fe0955948885691fcc14898bb57b5e583
Philip Meier <philip.meier06@googlemail.com>
2018-12-09 20:06:00 +0000

    Update e2e test with person page

--- cypress/integration/e2e_spec.js
+++ cypress/integration/e2e_spec.js
@@ -12,4 +12,20 @@ describe('Exploring the site', function() {
     cy.get('button').click();
     cy.wait('@apiCall');
   });
+
+  it('moves to person details when search result selected', () => {
+    cy.contains('div', 'Owen Lars').click();
+    cy.wait('@apiCall');
+    cy.url().should('include', '/person');
+  });
+
+  it('displays person details', () => {
+    cy.contains('h1', 'Owen Lars').should('be.visible');
+    cy.contains('.attribute', 'Height').children().last().should('not.to.be.empty');
+    cy.contains('.attribute', 'Mass').children().last().should('not.to.be.empty');
+    cy.contains('.attribute', 'Hair colour').children().last().should('not.to.be.empty');
+    cy.contains('.attribute', 'Eye colour').children().last().should('not.to.be.empty');
+    cy.contains('.attribute', 'Birth year').children().last().should('not.to.be.empty');
+    cy.contains('.attribute', 'Gender').children().last().should('not.to.be.empty');
+  });
 });
