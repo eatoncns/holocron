1b6a8e5bc3f476f38765b13221dfd725cfe31327
Philip Meier <philip.meier06@googlemail.com>
2018-12-10 14:01:13 +0000

    Update e2e test with starship display

--- cypress/integration/e2e_spec.js
+++ cypress/integration/e2e_spec.js
@@ -8,24 +8,31 @@ describe('Exploring the site', function() {
   it('starts on search page', function() {
     cy.visit('/');
     cy.get('input.search')
-      .type('lars');
+      .type('Luke');
     cy.get('button').click();
     cy.wait('@apiCall');
   });
 
   it('moves to person details when search result selected', () => {
-    cy.contains('div', 'Owen Lars').click();
+    cy.contains('div', 'Luke Skywalker').click();
     cy.wait('@apiCall');
     cy.url().should('include', '/person');
   });
 
   it('displays person details', () => {
-    cy.contains('h1', 'Owen Lars').should('be.visible');
+    cy.contains('h1', 'Luke Skywalker').should('be.visible');
     cy.contains('.attribute', 'Height').children().last().should('not.to.be.empty');
     cy.contains('.attribute', 'Mass').children().last().should('not.to.be.empty');
     cy.contains('.attribute', 'Hair colour').children().last().should('not.to.be.empty');
     cy.contains('.attribute', 'Eye colour').children().last().should('not.to.be.empty');
     cy.contains('.attribute', 'Birth year').children().last().should('not.to.be.empty');
     cy.contains('.attribute', 'Gender').children().last().should('not.to.be.empty');
+  });
+
+  it('displays starship details', () => {
+    cy.contains('.card', 'Starship').should('be.visible');
+    cy.contains('.card', 'Starship').click();
+    cy.wait('@apiCall');
+    cy.get('.card > p').should('not.to.be.empty');
   });
 });
